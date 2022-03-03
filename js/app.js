ffe7cc7032774013618cc16e8b6edf19bfc73b6f
mrsamirkhan <sirsamir@outlook.com>
2022-03-03 13:40:54 +0600

    fixed bg color

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ const apiKey = `e9524d8571b3d698b066eedf9ea2a45e`;
 //======================================
 
 const searchTemp = () => {
-    const city = document.getElementById('city-name').value;
+    const city = document.getElementById('city-name');
+    const cityField = city.value;
+    city.value = '';
+
     // console.log(city)
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityField}&appid=${apiKey}&units=metric`;
+    // console.log(url);
     fetch(url)
         .then(response => response.json())
         .then(data => displayTemp(data))
@@ -22,12 +25,13 @@ const setInnerText = (id, text) => {
 }
 
 const displayTemp = temp => {
+    const url = `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+    const icon = document.getElementById('weather-icon');
+    icon.setAttribute('src', url);
     setInnerText('city', temp.name);
     setInnerText('temperature', temp.main.temp);
     setInnerText('condition', temp.weather[0].main);
     setInnerText('description', temp.weather[0].description);
-    const url = `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
-    const icon = document.getElementById('weather-icon');
-    icon.setAttribute('src', url);
-    console.log(temp);
+
+    // console.log(temp);
 }
